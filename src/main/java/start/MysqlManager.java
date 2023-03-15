85f939f642ca7acd25ee3dccab9c350f106c3a38
shuning640 <21210240023@m.fudan.edu.cn>
2023-03-15 18:59:07 +0800

    init test ddj

--- src/main/java/start/MysqlManager.java
+++ src/main/java/start/MysqlManager.java
@@ -10,10 +10,10 @@ import java.util.*;
  */
 public class MysqlManager {
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    public final static String URL = "jdbc:mysql://localhost:3306/regression?useSSL=false&allowPublicKeyRetrieval" +
+    public final static String URL = "jdbc:mysql://10.176.34.99:3306/regression?useSSL=false&allowPublicKeyRetrieval" +
             "=true&characterEncoding=UTF8";
     public final static String NAME = "root";
-    public final static String PWD = "Aa110120";
+    public final static String PWD = "110120";
     private static Connection conn = null;
     private static Statement statement = null;
 
@@ -110,4 +110,31 @@ public class MysqlManager {
         return regressionMap;
     }
 
+    public static Map<String,List<Regression>> selectCleanRegressions(String sql) {
+        Map<String, List<Regression>> regressionMap = new HashMap<>();
+        try {
+            getStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            String projectName = "";
+            while (rs.next()) {
+                projectName = rs.getString("project_name");
+                Regression regression = new Regression();
+                regression.setId(rs.getString("id"));
+                regression.setProject_full_name(projectName);
+                regression.setBfc(rs.getString("bfc"));
+                regression.setBic(rs.getString("bic"));
+                regression.setWork(rs.getString("wc"));
+                regression.setTestCaseString(rs.getString("testcase1").split(";")[0]);
+                if (!regressionMap.containsKey(projectName)) {
+                    regressionMap.put(projectName, new LinkedList<>());
+                }
+                regressionMap.get(projectName).add(regression);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            closed();
+        }
+        return regressionMap;
+    }
 }
--- src/main/java/start/SourceManager.java
+++ src/main/java/start/SourceManager.java
@@ -11,8 +11,9 @@ import java.io.IOException;
  * @Description:
  */
 public class SourceManager {
-    private final static String metaProjectsDirPath = Main.workSpacePath+ File.separator + "meta_projects";
-    private final static String cacheProjectsDirPath = Main.workSpacePath+ File.separator+ "cache_projects";
+    private final static String metaProjectsDirPath = Main.workSpacePath + File.separator + "meta_projects";
+    private final static String cacheProjectsDirPath = Main.workSpacePath + File.separator+ "cache_projects";
+    private final static String saveDataPath = Main.workSpacePath+ File.separator+ "data";
 
     public static File checkout(String regressionID,Revision revision, File projectFile, String projectFullName) {
         //copy source code from meta project dir
@@ -46,7 +47,8 @@ public class SourceManager {
         String projectFullName = projectDirName.replace("_", "/");
         checkRequiredDir();
         File projectFile = new File(metaProjectsDirPath + File.separator + projectDirName);
-        if (projectFile.exists()) {
+        File[] child = projectFile.listFiles();
+        if (projectFile.exists() && child != null && child.length != 0) {
             return projectFile;
         } else {
             try {
@@ -80,5 +82,128 @@ public class SourceManager {
         } else {
             cacheProjectsDir.mkdirs();
         }
+    }
+
+    public static void saveData(File file, String newName) throws Exception{
+        FileUtils.copyFile(file,new File(saveDataPath + File.separator + newName));
+    }
+
+    public static void cleanCache() throws Exception{
+        FileUtils.cleanDirectory(new File(cacheProjectsDirPath));
+
+    }
+
+    public static void createShell(String regressionID, String projectFullName, Revision revision, String testcase,
+                            String errorType) {
+        String projectDirName = projectFullName.replace("/", "_");
+        File buildFile =
+                new File(cacheProjectsDirPath + File.separator + projectDirName + File.separator + regressionID+"_"+revision.getName(),
+                        "build.sh");
+        File testFile =
+                new File(cacheProjectsDirPath + File.separator + projectDirName + File.separator +  regressionID+"_"+revision.getName(),
+                        "test.sh");
+        if (!buildFile.exists()) {
+            try {
+                buildFile.createNewFile();
+                String s1 = "#!/bin/bash";
+                String s2 = "mvn clean compile test-compile &> /dev/null";
+                FileUtils.write(buildFile, s1 + "\n" + s2, "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (!testFile.exists()) {
+            try {
+                testFile.createNewFile();
+                String s1 = "#!/bin/bash" + "\n";
+                String s2 = "_OUT=$(timeout 300 mvn test -Dtest=" + testcase + " 2>&1)" + "\n";
+                String s3 = "SUCCESS=$(echo ${_OUT} | grep -c 'BUILD SUCCESS')" + "\n";
+                String s4 = "FAIL=$(echo ${_OUT} | grep -c '" + errorType + "')" + "\n";
+                String s5 = "if\n" +
+                        " [ \"${SUCCESS}\" = '1' ]; then\n" +
+                        "    PASS='1'\n" +
+                        "else\n" +
+                        "    PASS='0'\n" +
+                        "fi\n" +
+                        "\n" +
+                        "if [ ${PASS} = '1' -a ${FAIL} = '0' ]; then\n" +
+                        "    /bin/echo -n 'PASS'\n" +
+                        "elif [ ${FAIL} = '1' ]; then\n" +
+                        "    /bin/echo -n 'FAIL'\n" +
+                        "else\n" +
+                        "    /bin/echo -n 'UNRESOLVED'\n" +
+                        "fi";
+                FileUtils.write(testFile, s1 + s2 + s3 + s4 + s5, "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        buildFile.setExecutable(true,false);
+        buildFile.setReadable(true,false);
+        buildFile.setWritable(true,false);
+        testFile.setExecutable(true,false);
+        testFile.setReadable(true,false);
+        testFile.setWritable(true,false);
+    }
+
+    public static void createShell(String regressionID, String projectFullName, Revision revision, String testcase) {
+        String projectDirName = projectFullName.replace("/", "_");
+        File buildFile =
+                new File(cacheProjectsDirPath + File.separator + projectDirName + File.separator + regressionID+"_"+revision.getName(),
+                        "build.sh");
+        File testFile =
+                new File(cacheProjectsDirPath + File.separator + projectDirName + File.separator +  regressionID+"_"+revision.getName(),
+                        "test.sh");
+        if (!buildFile.exists()) {
+            try {
+                buildFile.createNewFile();
+                buildFile.canRead();
+                buildFile.canExecute();
+                buildFile.canWrite();
+                String s1 = "#!/bin/bash";
+                String s2 = "mvn clean compile test-compile &> /dev/null";
+                FileUtils.write(buildFile, s1 + "\n" + s2, "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (!testFile.exists()) {
+            try {
+                testFile.createNewFile();
+                String s1 = "#!/bin/bash" + "\n";
+                String s2 = "_OUT=$(timeout 300 mvn test -Dtest=" + testcase + " 2>&1)" + "\n";
+                String s3 = "SUCCESS=$(echo ${_OUT} | grep -c 'BUILD SUCCESS')" + "\n";
+                String s4 = "CE=$(echo ${_OUT} | grep -c 'COMPILATION ERROR')" + "\n";
+                String s5 = "if\n" +
+                        " [ \"${SUCCESS}\" = '1' ]; then\n" +
+                        "    PASS='1'\n" +
+                        "else\n" +
+                        "    PASS='0'\n" +
+                        "fi\n" +
+                        "\n" +
+                        "if [ ${PASS} = '1' -a ${CE} = '0' ]; then\n" +
+                        "    /bin/echo -n 'PASS'\n" +
+                        "elif [ ${CE} = '1' ]; then\n" +
+                        "    /bin/echo -n 'UNRESOLVED'\n" +
+                        "else\n" +
+                        "    /bin/echo -n 'FAIL'\n" +
+                        "fi";
+                FileUtils.write(testFile, s1 + s2 + s3 + s4 + s5, "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        buildFile.setExecutable(true,false);
+        buildFile.setReadable(true,false);
+        buildFile.setWritable(true,false);
+        testFile.setExecutable(true,false);
+        testFile.setReadable(true,false);
+        testFile.setWritable(true,false);
+    }
+
+    public static String getProjectCacheDir(String projectDirName){
+        return cacheProjectsDirPath + File.separator + projectDirName;
     }
 }
