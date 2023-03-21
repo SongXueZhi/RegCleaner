131aface4001a15d8d3b7b72f9517992afb75cdc
shuning640 <21210240023@m.fudan.edu.cn>
2023-03-21 16:48:01 +0800

    add multi-thread

--- src/main/java/start/SourceManager.java
+++ src/main/java/start/SourceManager.java
@@ -15,10 +15,37 @@ public class SourceManager {
     private final static String cacheProjectsDirPath = Main.workSpacePath + File.separator+ "cache_projects";
     private final static String saveDataPath = Main.workSpacePath+ File.separator+ "data";
 
+    public static File checkout(String regressionID,Revision revision, File projectFile, String message, String projectFullName) {
+        //copy source code from meta project dir
+        String projectDirName = projectFullName.replace("/", "_");
+        File projectCacheDir = new File(cacheProjectsDirPath + File.separator + message + File.separator, projectDirName);
+        if (projectCacheDir.exists() && !projectCacheDir.isDirectory()) {
+            projectCacheDir.delete();
+        }
+        projectCacheDir.mkdirs();
+
+        File revisionDir = new File(projectCacheDir,regressionID+"_"+revision.getName());
+        try {
+            if (revisionDir.exists()) {
+                FileUtils.forceDelete(revisionDir);
+            }
+            FileUtils.copyDirectoryToDirectory(projectFile, projectCacheDir);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+        new File(projectCacheDir, projectDirName).renameTo(revisionDir);
+        //git checkout
+        if (GitUtils.checkout(revision.getCommitID(), revisionDir)) {
+            return revisionDir;
+        }
+        return null;
+    }
+
     public static File checkout(String regressionID,Revision revision, File projectFile, String projectFullName) {
         //copy source code from meta project dir
         String projectDirName = projectFullName.replace("/", "_");
-        File projectCacheDir = new File(cacheProjectsDirPath + File.separator, projectDirName);
+        File projectCacheDir = new File(cacheProjectsDirPath + File.separator , projectDirName);
         if (projectCacheDir.exists() && !projectCacheDir.isDirectory()) {
             projectCacheDir.delete();
         }
@@ -88,9 +115,19 @@ public class SourceManager {
         FileUtils.copyFile(file,new File(saveDataPath + File.separator + newName));
     }
 
-    public static void cleanCache() throws Exception{
-        FileUtils.cleanDirectory(new File(cacheProjectsDirPath));
-
+    public static void getDDJResult(String message, String projectName, String newName) throws Exception{
+        File projectDir = new File(cacheProjectsDirPath + File.separator + message + File.separator + projectName);
+        String result = "";
+        File[] child = projectDir.listFiles();
+        for (File file : child) {
+            if (file.getName().contains("__CCA__")) {
+                File logFile = new File(file, "data.log");
+                if (logFile.exists()) {
+                    System.out.println("ddj result file :" + logFile);
+                    SourceManager.saveData(logFile, newName);
+                }
+            }
+        }
     }
 
     public static void createShell(String regressionID, String projectFullName, Revision revision, String testcase,
@@ -203,7 +240,63 @@ public class SourceManager {
         testFile.setWritable(true,false);
     }
 
-    public static String getProjectCacheDir(String projectDirName){
-        return cacheProjectsDirPath + File.separator + projectDirName;
+    public static void createShell(String regressionID, String message, String projectFullName, Revision revision, String testcase) {
+        String projectDirName = projectFullName.replace("/", "_");
+        File buildFile =
+                new File(cacheProjectsDirPath + File.separator + message + File.separator + projectDirName + File.separator + regressionID+"_"+revision.getName(),
+                        "build.sh");
+        File testFile =
+                new File(cacheProjectsDirPath + File.separator + message + File.separator + projectDirName + File.separator +  regressionID+"_"+revision.getName(),
+                        "test.sh");
+        if (!buildFile.exists()) {
+            try {
+                buildFile.createNewFile();
+                buildFile.canRead();
+                buildFile.canExecute();
+                buildFile.canWrite();
+                String s1 = "#!/bin/bash";
+                String s2 = "mvn clean compile test-compile &> /dev/null";
+                FileUtils.write(buildFile, s1 + "\n" + s2, "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (!testFile.exists()) {
+            try {
+                testFile.createNewFile();
+                String s1 = "#!/bin/bash" + "\n";
+                String s2 = "_OUT=$(timeout 300 mvn test -Dtest=" + testcase + " 2>&1)" + "\n";
+                String s3 = "SUCCESS=$(echo ${_OUT} | grep -c 'BUILD SUCCESS')" + "\n";
+                String s4 = "CE=$(echo ${_OUT} | grep -c 'COMPILATION ERROR')" + "\n";
+                String s5 = "if\n" +
+                        " [ \"${SUCCESS}\" = '1' ]; then\n" +
+                        "    PASS='1'\n" +
+                        "else\n" +
+                        "    PASS='0'\n" +
+                        "fi\n" +
+                        "\n" +
+                        "if [ ${PASS} = '1' -a ${CE} = '0' ]; then\n" +
+                        "    /bin/echo -n 'PASS'\n" +
+                        "elif [ ${CE} = '1' ]; then\n" +
+                        "    /bin/echo -n 'UNRESOLVED'\n" +
+                        "else\n" +
+                        "    /bin/echo -n 'FAIL'\n" +
+                        "fi";
+                FileUtils.write(testFile, s1 + s2 + s3 + s4 + s5, "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        buildFile.setExecutable(true,false);
+        buildFile.setReadable(true,false);
+        buildFile.setWritable(true,false);
+        testFile.setExecutable(true,false);
+        testFile.setReadable(true,false);
+        testFile.setWritable(true,false);
+    }
+
+    public static void cleanCache(String message) throws Exception{
+        FileUtils.cleanDirectory(new File(cacheProjectsDirPath + File.separator + message));
     }
 }
